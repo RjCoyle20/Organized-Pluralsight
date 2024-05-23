c40a282494b0033231fb8bc1e9b14bf87fc50de6
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-05-22 20:11:23 -0500

    Type conversion video

--- src/main/java/com/pluralsight/organized/Main.java
+++ src/main/java/com/pluralsight/organized/Main.java
@@ -4,23 +4,16 @@ package com.pluralsight.organized;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int valA=21;
-        int valB= 6;
-        int valC= 3;
-        int valD=1;
+        float floatval = 1.0f;
+        double doubleVal = 4.0d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long longVal = 5;
 
-        int result1 = valA-  valB / valC;
+        short result = (short) longVal;
+        short result2 = (short) (byteVal - longVal);
+        float result3 = longVal - floatval;
 
-        int result2 = (valA - valB) / valC;
-
-        System.out.println(result1);
-        System.out.println(result2);
-
-        int result3 =valA/valC * valD + valB;
-        int result4 = valA / (valC * (valD + valB));
-
-        System.out.println(result3);
-        System.out.println(result4);
 
     }
 }
